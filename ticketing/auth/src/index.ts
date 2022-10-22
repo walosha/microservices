1c67a77a76acbee5766b9c896b0806f69f589cb2
olawale afuye <walosha@yahoo.com>
2022-10-22 12:00:19 +0100

    init commit

---
+++ ticketing/auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { json } from "body-parser";
+
+const app = express();
+app.use(json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("this is the current user route!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
+});
